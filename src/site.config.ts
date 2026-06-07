54e30c7d5295b6234340eb78fff058d86e2e7b75
linnene <2691234891@qq.com>
2026-06-07 13:06:48 +0800

    Adjust layout and add photography section

--- src/site.config.ts
+++ src/site.config.ts
@@ -11,6 +11,6 @@ export const site = {
     ['Blog', '/blog/'],
     ['Roadmap', '/roadmap/'],
     ['Projects', '/projects/'],
-    ['About', '/about/'],
+    ['Photography', '/photography/'],
   ],
 };
